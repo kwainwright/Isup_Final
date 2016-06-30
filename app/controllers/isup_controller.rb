76c9c36b5a4579bb69bcf10e621d143fb756ba20
unknown <kwainwri@.njmrsnet-aegis.com>
2016-06-30 18:42:58 -0400

    Some initial link validation

--- app/controllers/isup_controller.rb
+++ app/controllers/isup_controller.rb
@@ -40,7 +40,48 @@ end
   
   def validate
     @url = params[:url]
-    format.html {render :failure}
+    
+    if url_exist?(@url)
+      @status = "Available"
+    else
+      @status = "Not Available"
+    end
+    #format.html {render :failure}
   end
+  
+  protected
+  
+   require "net/http"
+  
+   def url_exist?(url_string)
+    url = URI.parse(url_string)
+    url.port = 80
+    http = Net::HTTP.start(url.host, url.port, {open_timeout: 5, read_timeout: 5})
+    response = http.head("/")
+    response.code == "200"
+    rescue Timeout::Error, SocketError
+      false
+   end
+ #   req = Net::HTTP.new(url.host, url.port)
+ #   res = req.request_head(url.path)
+ #   if res == "200"
+ #     true
+ #   else
+ #     false
+ #   end
+ #   rescue
+ #     false
+ #  end
+    #req.use_ssl = (url.scheme == 'https')
+    #path = url.path if url.path.present?
+    #res = req.request_head(path || '/')
+    #if res.kind_of?(Net::HTTPRedirection)
+    #  url_exist?(res['location']) # Go after any redirect and make sure you can access the redirected URL 
+    #else
+    #  ! %W(4 5).include?(res.code[0]) # Not from 4xx or 5xx families
+    #end
+    #rescue Errno::ENOENT
+    # false #false if can't find the server
+    #end
   
 end
